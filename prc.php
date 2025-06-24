e13a183be0c3ba7ca05595a4521e632fb78038d7
João Vitor N. Saravalli <jv.saravalli@gmail.com>
2025-06-24 11:57:05 -0300

    segunda mudança

---
+++ prc.php
@@ -0,0 +1,15 @@
+<?php 
+if ($_SESSION["REQUEST_METHOD"] == "POST") {
+        $nome = $_POST["nome"] ?? "visitante";
+        $email = $_POST["email"];
+        $senha = $_POST["senha"];
+}
+
+    if ($email == "teste@exmpl.com" && $senha == "teste123") {
+        header("Location: cmpr.html");
+        exit;
+        } else {
+            echo "Email ou senha inválidos. Tente novamente.";
+}
+
+?>
